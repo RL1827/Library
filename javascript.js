414c2fb30878554289c888770280a9c47431de04
RL <renalow37@gmail.com>
2026-09-21 16:11:56 +0800

    Refactored the Library Book class

--- javascript.js
+++ javascript.js
@@ -1,21 +1,99 @@
 
 const Library = []
 
-function Book(author, title, pages, status){
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.status = status
-    this.id = crypto.randomUUID()
-}
-
 let displayCard  = document.querySelector(".display")
-Book.prototype.toString = function(){
-    return `${this.author}, {this.title}, ${this.pages}, ${this.status}, ${this.id}`
-}
 
-Book.setStatus = function(status){
-    this.status = status
+class Book{
+    constructor(author, title, pages, status){
+        this._author = author
+        this._title = title
+        this._pages = pages
+        this._status = status
+        this._id = crypto.randomUUID()
+    }
+
+    createCards(book){
+    
+        let parentCard = document.createElement("div")
+        let titleText = document.createElement("p")
+        titleText.textContent = `Title: ${book.title}`
+        let authorText = document.createElement("p")
+        authorText.textContent = `Author: ${book.author}`
+        let pageText = document.createElement("p")
+        pageText.textContent = `Number of Pages: ${book.pages}`
+        let statusText = document.createElement("p")
+        statusText.textContent = `Reading Status: ${book.status}`
+        let idText = document.createElement("p")
+        idText.textContent = `ID: ${book.id}`
+        let removeButton = document.createElement("button")
+        removeButton.textContent = "Remove Book"
+        removeButton.addEventListener("click", (event)=>{
+            let parent= event.target.parentElement
+            let parentID = parent.getAttribute("id")
+            for (let i = Library.length - 1; i >= 0; i--) {
+                if (Library[i].id === parentID) {
+                    Library.splice(i, 1); // Removes 1 element at index i
+                }
+            }
+            parent.remove()
+            }
+        )
+        let toggleRead = document.createElement("button")
+        toggleRead.textContent = "Toggle Read"
+        toggleRead.addEventListener("click", ()=>{
+            if (book.status === "not-read"){
+                book.status = "ongoing"
+            }else if (book.status === "ongoing"){
+                book.status = "read"
+            } else{
+                book.status = "not-read"
+            }
+            statusText.textContent = `Reading Status: ${book.status}`
+            console.log(book)
+        })
+        parentCard.appendChild(titleText)
+        parentCard.appendChild(authorText)
+        parentCard.appendChild(pageText)
+        parentCard.appendChild(statusText)
+        parentCard.appendChild(idText)
+        parentCard.appendChild(removeButton)
+        parentCard.appendChild(toggleRead)
+        
+
+        parentCard.classList.add("cardDiv")
+        parentCard.setAttribute('id', `${book.id}`)
+        titleText.classList.add("cardText")
+        authorText.classList.add("cardText")
+        pageText.classList.add("cardText")
+        statusText.classList.add("cardText")
+        idText.classList.add("cardText")
+
+        return parentCard
+    }
+
+    set status(status){
+        this._status = status
+    }
+
+    get status(){
+        return this._status
+    }
+
+    get author(){
+        return this._author
+    }
+
+    get title(){
+        return this._title
+    }
+
+    get pages(){
+        return this._pages
+    }
+
+    get id(){
+        return this._id
+    }
 }
 
 let listening = document.querySelector("#bookform")
@@ -52,67 +130,9 @@ listening.addEventListener("submit", (event)=> {
     let newBook = new Book(authorname, titlename,numberofpages,readstatus)
     console.log(newBook)
     Library.push(newBook)
-    let child = createCards(newBook)
+    let child = newBook.createCards(newBook)
     displayCard.appendChild(child)
     dialog.hidePopover()
 })
 
 
-function createCards(book){
-    
-    parentCard = document.createElement("div")
-    titleText = document.createElement("p")
-    titleText.textContent = `Title: ${book.title}`
-    authorText = document.createElement("p")
-    authorText.textContent = `Author: ${book.author}`
-    pageText = document.createElement("p")
-    pageText.textContent = `Number of Pages: ${book.pages}`
-    statusText = document.createElement("p")
-    statusText.textContent = `Reading Status: ${book.status}`
-    idText = document.createElement("p")
-    idText.textContent = `ID: ${book.id}`
-    removeButton = document.createElement("button")
-    removeButton.textContent = "Remove Book"
-    removeButton.addEventListener("click", (event)=>{
-        let parent= event.target.parentElement
-        let parentID = parent.getAttribute("id")
-        for (let i = Library.length - 1; i >= 0; i--) {
-            if (Library[i].id === parentID) {
-                Library.splice(i, 1); // Removes 1 element at index i
-            }
-        }
-        parent.remove()
-        }
-    )
-    toggleRead = document.createElement("button")
-    toggleRead.textContent = "Toggle Read"
-    toggleRead.addEventListener("click", ()=>{
-        if (book.status === "not-read"){
-            book.status = "ongoing"
-        }else if (book.status === "ongoing"){
-            book.status = "read"
-        } else{
-            book.status = "not-read"
-        }
-        statusText.textContent = `Reading Status: ${book.status}`
-        console.log(book)
-    })
-    parentCard.appendChild(titleText)
-    parentCard.appendChild(authorText)
-    parentCard.appendChild(pageText)
-    parentCard.appendChild(statusText)
-    parentCard.appendChild(idText)
-    parentCard.appendChild(removeButton)
-    parentCard.appendChild(toggleRead)
-    
-
-    parentCard.classList.add("cardDiv")
-    parentCard.setAttribute('id', `${book.id}`)
-    titleText.classList.add("cardText")
-    authorText.classList.add("cardText")
-    pageText.classList.add("cardText")
-    statusText.classList.add("cardText")
-    idText.classList.add("cardText")
-
-    return parentCard
-}
